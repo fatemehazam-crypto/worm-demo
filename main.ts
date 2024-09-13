53a121c6fc1b143436950605b0030b8c7d3cece4
Eric Iniguez <43428465+colonket@users.noreply.github.com>
2024-09-13 16:51:35 -0500

    Overhaul into working prototype

--- main.ts
+++ main.ts
@@ -1,11 +1,37 @@
+function become_infected () {
+    if (!(i_am_infected)) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.mysterious), music.PlaybackMode.UntilDone)
+        i_am_infected = true
+    }
+    for (let n of my_neighbors) {
+        basic.pause(2000)
+        radio.sendString("" + (n))
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    become_infected()
+})
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-    radio.sendString(receivedString)
+    if (receivedString == my_id) {
+        become_infected()
+    }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendString(">:)")
+input.onGesture(Gesture.Shake, function () {
+    i_am_infected = false
+    basic.showIcon(IconNames.Happy)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
 })
+let my_neighbors: string[] = []
+let my_id = ""
+let i_am_infected = false
 radio.setGroup(1)
+i_am_infected = false
+my_id = "your_name_here"
+my_neighbors = ["neighbor_id_here"]
 basic.forever(function () {
-	
+    if (i_am_infected) {
+        basic.showIcon(IconNames.Skull)
+    } else {
+        basic.showString(my_id)
+    }
 })
