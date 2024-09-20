f52eb64393008cb2197c08bde8fbefd9f84e34ae
Eric Iniguez <43428465+colonket@users.noreply.github.com>
2024-09-20 13:08:52 -0500

    Tune infection behavior for better user interactions

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function become_infected () {
     if (!(i_am_infected)) {
-        music.play(music.builtinPlayableSoundEffect(soundExpression.mysterious), music.PlaybackMode.UntilDone)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.mysterious), music.PlaybackMode.InBackground)
         i_am_infected = true
     }
     for (let n of my_neighbors) {
@@ -8,6 +8,7 @@ function become_infected () {
         radio.sendString("" + (n))
     }
 }
+// Infect your Micro:Bit!
 input.onButtonPressed(Button.AB, function () {
     become_infected()
 })
@@ -16,10 +17,11 @@ radio.onReceivedString(function (receivedString) {
         become_infected()
     }
 })
+// Cure your Micro:Bit!
 input.onGesture(Gesture.Shake, function () {
     i_am_infected = false
     basic.showIcon(IconNames.Happy)
-    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.InBackground)
 })
 let my_neighbors: string[] = []
 let my_id = ""
@@ -27,7 +29,7 @@ let i_am_infected = false
 radio.setGroup(1)
 i_am_infected = false
 my_id = "your_name_here"
-my_neighbors = ["neighbor_id_here"]
+my_neighbors = ["neighbor1", "neighbor2"]
 basic.forever(function () {
     if (i_am_infected) {
         basic.showIcon(IconNames.Skull)
